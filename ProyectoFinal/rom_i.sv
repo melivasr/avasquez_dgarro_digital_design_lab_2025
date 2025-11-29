8edad76aa960e67da3c3d93b8bb8ecadb6f4b29d
melivasr <131542698+melivasr@users.noreply.github.com>
2025-11-29 02:26:09 -0600

    cambios rom y key mapper

--- ProyectoFinal/rom_i.sv
+++ ProyectoFinal/rom_i.sv
@@ -1,12 +1,19 @@
 module rom_i (
-    input  logic [6:0] address, // 7 bits 128 posiciones
+    input  logic [6:0]  address,  // 128 palabras (0..127)
     output logic [31:0] q
 );
-    logic [31:0] mem [0:127]; // 0..127
 
+    // Para FPGA
+    (* ram_init_file = "program.hex" *)
+    logic [31:0] mem [0:127];
+
+`ifndef SYNTHESIS
+    // Solo para simulación (ModelSim)
     initial $readmemh("program.hex", mem);
+`endif
 
     always_comb begin
         q = mem[address];
     end
+
 endmodule
